8fa6b19c109a9ef0b4cddf45a07f17fe779938e3
吴俊杰 <20717@etransfar.com>
2022-04-18 15:25:49 +0800

    react-debugger 第一次提交

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
+<!--
+ * @Author: your name
+ * @Date: 2022-04-18 15:12:15
+ * @LastEditTime: 2022-04-18 15:25:21
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /个人工作/react-debugger/README.md
+-->
+
 # react-debugger
+
+## 前言
+
+> 为了阅读 React 源码搭建的调试环境，react 包已经引入至 src 目录下，可以直接改源码调试，非常便捷。具体的搭建过程可以看此文章[《React18 源码弃坑指南（一）-- 调试环境搭建
+> 》](https://juejin.cn/post/7080344211987365925)。
+
+## 起飞
+
+```
+yarn
+
+yarn start
+```
---
+++ src/index.js
@@ -0,0 +1,25 @@
+/*
+ * @Author: your name
+ * @Date: 2022-03-28 18:49:15
+ * @LastEditTime: 2022-03-28 19:08:02
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /个人工作/react-dev/src/index.js
+ */
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
